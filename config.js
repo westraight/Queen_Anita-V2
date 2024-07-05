cefe213e4cdc9ababdafefff991a43dcd0ccfcf6
westraight <158785034+westraight@users.noreply.github.com>
2024-07-06 01:48:17 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,15 +4,15 @@ if (fs.existsSync(".env"))
   require("dotenv").config({ path: __dirname + "/.env" });
 
 //=======[dependencies]====================//
-global.SESSION_ID = process.env.SESSION_ID || "";
-global.MONGODB = process.env.MONGODB_URI || "";
+global.SESSION_ID = process.env.SESSION_ID || "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiU0hCSjB0a1Arc0hCWHV2MTFCSEFNbGtoOHBLQlBsYUtHL3R2RFo1RjdIUT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTXhKSHlseXdHQVNGeWI5Z1U5TkxmeFlJMFR1N0ZUcFFaMWRYMkxPNUxUMD0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ1TnFRZUNGVlo4U3M2OXR3N3o5N1RPRXVUc29TcDBTUTRWRHYwNll3dkdvPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJJZXlBQ3NRNUJ3MWEzN1pjdVZVTUZqVUkydlhBODhpTUxWcWtFbVNlOVVFPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IldNOUViYWNUbnV4NVJMZFNYMGR2WVNpTGpGbW9hUEVBU0ROSDkvQk1OSFk9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ino3NzhrZUxTK3RyNmRGSnBYVnBvYjF2cFRuTklnMmVvTHRvKy9waVg1RWM9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiME5KVk14anFuYjd6ZlNyQS8xRmZxYVlLMGxoeXA4aGE5bXBZeElGVm8xRT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSDgrWWtUbFh1ZG55ZmVIV2dqZGxrRHNwSkdMUWxXWU9vSFk3em9FMFJnVT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlF1RmxRNnNyUmwxV2N2WG1LVUJtdXQrZVl0bTl3M0l6dXc5cmJTS3J5Vm04cmxOUXpYaVIvbFR2ZXRBbzJzMmdROUdHMDdQVmxEcXg1T3ZqUVE3cERnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6NzgsImFkdlNlY3JldEtleSI6IjR0RHhzWUtSQVlIRUgvNk1ENEZJcG8yRDRnMXdnSExvaEhlQUxBUEszc289IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6IjQzZXB5dk0wUlJTaGhneG1mU2FCWGciLCJwaG9uZUlkIjoiZDQzNTJlMTMtMGIxMi00YTZmLWE3YzAtYTA4MTIzNzczNWU2IiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImxTbGk4NjZPQkZEUTZzMG9MaXVmaCtvaDZlMD0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJSdzNYc2RoV2Rkc2dvYjBUV3M3SmlnczlJTWc9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiTUdLQjZBNVMiLCJtZSI6eyJpZCI6IjI2MDc2NTE5OTE3NTo2M0BzLndoYXRzYXBwLm5ldCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDTjMvbnM4Q0VJQ0ZvclFHR0FrZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiSXRyZE5zaVBiVlVHTUNDNklpMjh3OFM5U0lZU2o5bWRMZ0FhT3ZuVmYzWT0iLCJhY2NvdW50U2lnbmF0dXJlIjoiVmppZm9GV1RsQXcwbllCS0duaG94ZUZQYmc3QjFpL2Q2elJmZVhLVFNoczRGdjI5WVNpK0NQd3crR0NJbW0reFBOaW9Eb3phWUxOSVJ6S01LMFM5QlE9PSIsImRldmljZVNpZ25hdHVyZSI6IjQzYzR2bmI1SlVXaW1Kc0gyVDdiQTVFMlloUkFjYlI1TGlTSnh0d3ZUMXcvNGs3OGRBVkJod2NKb0tudnhDaDlYalVrL0FQQ2Z1UWMvNUV0dnArbEF3PT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjYwNzY1MTk5MTc1OjYzQHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQlNMYTNUYklqMjFWQmpBZ3VpSXR2TVBFdlVpR0VvL1puUzRBR2pyNTFYOTIifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3MjAyMjIzNTB9";
+global.MONGODB = process.env.MONGODB_URI || "mongodb+srv://thembasakala98:themba2006@cluster0.zft1ycs.mongodb.net/?retryWrites=true&w=majority";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
 global.sudo = process.env.SUDO
   ? process.env.SUDO.replace(/[\s+]/g, "")
   : "null";
 global.owner = process.env.OWNER_NUMBER
   ? process.env.OWNER_NUMBER.replace(/[\s+]/g, "")
-  : "2349066528353";
+  : "260765199175";
 global.THUMB_IMAGE =
   process.env.THUMB_IMAGE ||
   process.env.IMAGE ||
@@ -27,11 +27,11 @@ module.exports = {
   HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "`©QUEEN_ANITA-V2`",
+  caption: process.env.CAPTION || "4L Draco ❤️🔞",
   author: process.env.PACK_AUTHER || "QUEEN_ANITA-V2",
   packname: process.env.PACK_NAME || "A N I T A",
-  botname: process.env.BOT_NAME || "QUEEN_ANITA-V2",
-  ownername: process.env.OWNER_NAME || "David Cyril",
+  botname: process.env.BOT_NAME || "We Straight ❤️🔞",
+  ownername: process.env.OWNER_NAME || "4L Draco ❤️🔞",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
@@ -42,7 +42,7 @@ module.exports = {
   HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
   aitts_Voice_Id: process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
-  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "public",
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
   LANG: (process.env.THEME || "WhatsApp").toUpperCase(),
 };
 global.port = process.env.PORT;
@@ -51,7 +51,7 @@ global.email = "davidcyril209@gmail.com";
 global.location = "";
 global.allowJids = process.env.ALLOW_JID || "null";
 global.blockJids = process.env.BLOCK_JID || "null";
-global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Lagos";
+global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Lusaka";
 global.github = process.env.GITHUB || "https://github.com/DeeCeeXxx/QUEEN_Anita-V2";
 global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L";
 global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L";
@@ -64,7 +64,7 @@ global.warncount = process.env.WARN_COUNT || 3;
 global.disablepm = process.env.DISABLE_PM || "false";
 (global.disablegroup = process.env.DISABLE_GROUPS || "false"),
   (global.MsgsInLog = process.env.MSGS_IN_LOG || "true");
-global.waPresence = process.env.WAPRESENCE || "online";
+global.waPresence = process.env.WAPRESENCE || "recording";
 global.readcmds = process.env.READ_COMMAND || "false";
 global.readmessage = process.env.READ_MESSAGE || "false";
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null";
